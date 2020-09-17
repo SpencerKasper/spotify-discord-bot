1cade2863cc307bb985db21877cd2ea337da03b3
Spencer Kasper <spencer.kasper@gmail.com>
2020-09-17 18:42:24 -0500

    * Refactor the DiscordUser class a bit

--- utils/DiscordUser.ts
+++ utils/DiscordUser.ts
@@ -1,13 +1,18 @@
 export class DiscordUser {
     static getUsername(msg, client) {
-        var user = msg.member;
-        user = user.toString();
+        const user = msg.member.toString();
+        let userSearchCode;
+
         if (user.includes("!")) {
-            user = user.split("!")[1].split(">")[0];
+            userSearchCode = this.extractUserSearchCode(user, '!');
         } else {
-            user = user.split("@")[1].split(">")[0];
+            userSearchCode = this.extractUserSearchCode(user, '@');
         }
 
-        return client.users.get(user).username;
+        return client.users.get(userSearchCode).username;
+    }
+
+    private static extractUserSearchCode(user: string, separator) {
+        return user.split(separator)[1].split(">")[0];
     }
 }
